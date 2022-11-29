c8b6d3f8b9d4d92199841a4c4805214bcb8f2c8a
rojhat <2479009o@student.gla.ac.uk>
2022-11-29 15:17:00 +0000

    changes to a lot of stuff still to add aria!

--- ScriptAddJournal.js
+++ ScriptAddJournal.js
@@ -69,28 +69,7 @@ function deleteNote(noteId) {
     showNotes();
 }
 
-function myFunctions() {
-    location.replace("accessible.html")
-  }
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+
 
 function updateNote(noteId, title, filterDesc) {
     let description = filterDesc.replaceAll('<br/>', '\r\n');
